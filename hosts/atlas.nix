03b34ea8fb9aca388d93ff73bfdb84c53971ee22
atlas <kefirzefir523@gmail.com>
2025-06-08 12:03:24 +0500

    init

--- hosts/atlas.nix
+++ hosts/atlas.nix
@@ -26,7 +26,7 @@ nixpkgs.config.allowUnfree = true;
     trusted-public-keys = [ "cache.nixos.org-1:LfQJbNa3Y2Sb+w6G8H9hTf6LgNfK3RM7um6KFB2z3nM=" ];
   };
 
-{
+
   services.xserver = {
     enable = true;
     layout = null; # убираем, чтобы не конфликтовало
@@ -35,7 +35,7 @@ nixpkgs.config.allowUnfree = true;
       options = "grp:alt_shift_toggle";
     };
   };
-}
+
 
 
 environment.shellAliases = {
